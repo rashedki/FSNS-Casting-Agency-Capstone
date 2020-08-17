89b4bbbaef77c3b2a9eb8c3c142a368c49aca669
Khalil Al-Rashid <rashedki@hotmail.com>
2020-08-17 14:28:07 +0300

    Adding new movie endpoint
    Creating an endpoint to allow a new movie to be added

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import os
 from flask import Flask, request, abort, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+from datetime import datetime
 
 #importing objects from other files in this repo.
 from auth import AuthError, requires_auth
@@ -57,6 +58,35 @@ def create_app(test_config=None):
           'success': True,
           'actors': actors
       })
+  
+  # Creating an endpoint to allow a new movie to be added
+  @app.route('/movies', methods = ['POST'])
+  @requires_auth('add:movies')
+  def add_movie():
+      # Getting information from request body
+      body = request.get_json()
+
+      # Extracting information from body.
+      movie_title = body.get('title')
+      movie_release_date = body.get('release_date')
+
+      # Checking to see if proper info is present
+      if None in (title and release_date):
+          abort(422)
+
+      try:
+          # Adding new movie object with request body info
+          movie = Movie(title = movie_title, release_date=datetime.strptime(movie_release_date, '%Y-%m-%d'))
+          movie.insert()
+
+          # Returning success information
+          return jsonify({
+              'success': True,
+              'movie_id': movie.id,
+              'movies': [movie.format()],
+          })
+      except:
+          abort(422)
 
   return app
 
