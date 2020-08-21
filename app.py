eed69fd6bf5d44b9cfbba152719fe4e419e4dbec
Khalil Al-Rashid <rashedki@hotmail.com>
2020-08-21 14:04:03 +0300

    Adding CORS Headers

--- app.py
+++ app.py
@@ -14,6 +14,12 @@ def create_app(test_config=None):
     setup_db(app)
     CORS(app, resources = {r"/api/": {"origins": "*"}})
 
+    # CORS Headers
+    @app.after_request
+    def after_request(response):
+        response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization,true')
+        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE')
+        return response
 
     # Home greeting test
         
