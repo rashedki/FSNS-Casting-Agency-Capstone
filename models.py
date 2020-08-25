a2c4503b728232efc0705c05648aa5ee63f4c984
Khalil Al-Rashid <rashedki@hotmail.com>
2020-08-25 20:41:13 +0300

    updating the model file.
    this model file making problems for me. it didn't allow the delete to be done. I will update it know to check my other solution

--- models.py
+++ models.py
@@ -21,6 +21,18 @@ def setup_db(app, database_path=database_path):
     db.init_app(app)
     db.create_all()
 
+'''
+db_drop_and_create_all()
+    drops the database tables and starts fresh
+    can be used to initialize a clean database
+    !!NOTE you can change the database_filename variable to
+    have multiple verisons of a database
+'''
+
+
+def db_drop_and_create_all():
+    db.drop_all()
+    db.create_all()
 
 # Creating a Movies class object to hold / update information about movies
 class Movie(db.Model):
@@ -38,7 +50,7 @@ class Movie(db.Model):
     # Creating an insert function
     def insert(self):
         db.session.add(self)
-        db.session.commit(self)
+        db.session.commit()
 
     # Creating an update function
     def update(self):
@@ -55,7 +67,6 @@ class Movie(db.Model):
             'id': self.id,
             'title': self.title,
             'release_date': self.release_date,
-            'actors': self.actors
         }
 
 # Creating an Actor class object to hold / update information about actors & actresses
@@ -70,15 +81,15 @@ class Actor(db.Model):
     gender = db.Column(db.String)
 
     # Connecting movie to the respective actors from the movies table
-    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id'), nullable = False)
+    movie_id = db.Column(db.Integer, db.ForeignKey('movies.id', ondelete='SET NULL'), nullable=True)
     
-    # To access the actor list of movies
-    movies = db.relationship('Movie', backref = 'actors')
+    # # To access the actor list of movies
+    # movies = db.relationship('Movie', backref = 'actors')
     
     # Creating an insert function
     def insert(self):
         db.session.add(self)
-        db.session.commit(self)
+        db.session.commit()
 
     # Creating an update function
     def update(self):
