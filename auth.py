e02d91461eb3748a2f438e2e637ceca3ba0321b8
Khalil Al-Rashid <rashedki@hotmail.com>
2020-08-27 15:58:14 +0300

    adding the auth0 setting as environment variables as requested by reviewer

--- auth.py
+++ auth.py
@@ -4,11 +4,12 @@ from flask import request, _request_ctx_stack, abort
 from functools import wraps
 from jose import jwt
 from urllib.request import urlopen
+import os
 
 # Defining Autho0 information
-AUTH0_DOMAIN = 'rashedki.us.auth0.com'
-ALGORITHMS = ['RS256']
-API_AUDIENCE = 'capstone'
+AUTH0_DOMAIN = os.environ.get('AUTH0_DOMAIN')
+ALGORITHMS = os.environ.get('ALGORITHMS')
+API_AUDIENCE = os.environ.get('API_AUDIENCE')
 
 ## AuthError Exception
 '''
